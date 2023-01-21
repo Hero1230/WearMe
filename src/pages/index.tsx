bf62ab462743cedaf41e662709ff7e5eb9f8d4d9
Max Mogilski <maksmogi@gmail.com>
2023-01-22 00:50:04 +0100

    Created navigation, and itemsList component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,9 @@
-import ItemCard from "@/components/ItemCard";
+import ItemsList from "@/components/ItemsList";
 
 export default function Home() {
 	return (
-		<div className="flex justify-center h-screen w-screen gap-1 flex-wrap p-2">
-			<ItemCard
-				title="Nike Jordan"
-				description="If you are looking for a modern sneaker that is easy to style, or are you just looking for a shoe for the coming summer, then this sneaker is just for you!"
-				price={200}
-			/>
-			<ItemCard
-				title="Nike Jordan"
-				description="If you are looking for a modern sneaker that is easy to style, or are you just looking for a shoe for the coming summer, then this sneaker is just for you!"
-				price={200}
-			/>
-			<ItemCard
-				title="Nike Jordan"
-				description="If you are looking for a modern sneaker that is easy to style, or are you just looking for a shoe for the coming summer, then this sneaker is just for you!"
-				price={200}
-			/>
-			<ItemCard
-				title="Nike Jordan"
-				description="If you are looking for a modern sneaker that is easy to style, or are you just looking for a shoe for the coming summer, then this sneaker is just for you!"
-				price={200}
-			/>
-			<ItemCard
-				title="Nike Jordan"
-				description="If you are looking for a modern sneaker that is easy to style, or are you just looking for a shoe for the coming summer, then this sneaker is just for you!"
-				price={200}
-			/>
-			<ItemCard
-				title="Nike Jordan"
-				description="If you are looking for a modern sneaker that is easy to style, or are you just looking for a shoe for the coming summer, then this sneaker is just for you!"
-				price={200}
-			/>
+		<div className="flex justify-center w-screen m-4">
+			<ItemsList />
 		</div>
 	);
 }
