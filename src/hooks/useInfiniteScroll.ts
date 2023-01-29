8d65726d93e274f2575f134f8b57d2855507dce3
Max Mogilski <maksmogi@gmail.com>
2023-01-29 22:48:24 +0100

    Small change to make production work without errors

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,62 +1,62 @@
 import { useState, useEffect } from "react";
-import db from "firebase/firestore";
+// import db from "firebase/firestore";
 
-interface Item {
-	id: string;
-	name: string;
-	// other properties
-}
+// interface Item {
+// 	id: string;
+// 	name: string;
+// 	// other properties
+// }
 
-interface UseInfiniteScroll {
-	data: Item[];
-}
+// interface UseInfiniteScroll {
+// 	data: Item[];
+// }
 
-function useInfiniteScroll(collectionName: string): UseInfiniteScroll {
-	const [data, setData] = useState<Item[]>([]);
-	const [lastVisible, setLastVisible] =
-		useState<firestore.QueryDocumentSnapshot<firestore.DocumentData> | null>(
-			null
-		);
-	const limit = 10;
+// function useInfiniteScroll(collectionName: string): UseInfiniteScroll {
+// 	const [data, setData] = useState<Item[]>([]);
+// 	const [lastVisible, setLastVisible] =
+// 		useState<firestore.QueryDocumentSnapshot<firestore.DocumentData> | null>(
+// 			null
+// 		);
+// 	const limit = 10;
 
-	useEffect(() => {
-		getData();
-		// Add scroll event listener
-		window.addEventListener("scroll", handleScroll);
-		return () => {
-			// Remove scroll event listener
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+// 	useEffect(() => {
+// 		getData();
+// 		// Add scroll event listener
+// 		window.addEventListener("scroll", handleScroll);
+// 		return () => {
+// 			// Remove scroll event listener
+// 			window.removeEventListener("scroll", handleScroll);
+// 		};
+// 	}, []);
 
-	function handleScroll() {
-		if (
-			window.innerHeight + document.documentElement.scrollTop ===
-			document.documentElement.offsetHeight
-		) {
-			getData();
-		}
-	}
+// 	function handleScroll() {
+// 		if (
+// 			window.innerHeight + document.documentElement.scrollTop ===
+// 			document.documentElement.offsetHeight
+// 		) {
+// 			getData();
+// 		}
+// 	}
 
-	function getData() {
-		let query = db
-			.collection(collectionName)
-			.orderBy("timestamp", "desc")
-			.limit(limit);
+// 	function getData() {
+// 		let query = db
+// 			.collection(collectionName)
+// 			.orderBy("timestamp", "desc")
+// 			.limit(limit);
 
-		if (lastVisible) {
-			query = query.startAfter(lastVisible);
-		}
+// 		if (lastVisible) {
+// 			query = query.startAfter(lastVisible);
+// 		}
 
-		query.get().then((querySnapshot) => {
-			let newData = querySnapshot.docs.map((doc) => ({
-				...doc.data(),
-				id: doc.id,
-			}));
-			setData([...data, ...newData]);
-			setLastVisible(querySnapshot.docs[querySnapshot.docs.length - 1]);
-		});
-	}
+// 		query.get().then((querySnapshot) => {
+// 			let newData = querySnapshot.docs.map((doc) => ({
+// 				...doc.data(),
+// 				id: doc.id,
+// 			}));
+// 			setData([...data, ...newData]);
+// 			setLastVisible(querySnapshot.docs[querySnapshot.docs.length - 1]);
+// 		});
+// 	}
 
-	return { data };
-}
+// 	return { data };
+// }
